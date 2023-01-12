86ccec982a2f9fdefe2796fedbf22d84647a9fd3
kudinroman <kudinroman82@gmail.com>
2023-01-12 18:35:03 +0300

    upgrade user form, add menu

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,6 +44,6 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :birth_date)
+      params.require(:user).permit(:first_name, :last_name, :birth_date, :role, :is_active)
     end
 end
--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    plugins: {
-        "postcss-import": {}, // <= Add this
-        tailwindcss: {},
-        autoprefixer: {}
-    }
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ]
 }
